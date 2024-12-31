7f665b0409f64e4d64e646a9a47b08ceb2311ae5
rauulssanchezz <rauulssanchezz@gmail.com>
2024-12-31 10:40:21 +0100

    header, home creation and home in app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo:'home-page', pathMatch: 'full' },
+  { path:'home-page', component: HomePageComponent  }
+];
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomePageComponent {
+
+}
