f91db09238a7e763e35342e06246e3c543cd84bc
rauulssanchezz <rauulssanchezz@gmail.com>
2024-12-31 13:33:59 +0100

    onSearch with filteredCountries

--- src/app/components/country-selector/country-selector.component.ts
+++ src/app/components/country-selector/country-selector.component.ts
@@ -11,23 +11,25 @@ import { RestCountriesService } from '../../services/rest-countries.service';
 })
 export class CountrySelectorComponent {
   countries: any[] = [];
+  filteredCountries: any[] = [];
 
   constructor(private restCountriesService: RestCountriesService) {
     this.restCountriesService.getAllCountries().subscribe((data: any[]) => {
       console.log(data);
       this.countries = data;
+      this.filteredCountries = data;
     });
   }
 
   onSearch(search: Event){
     const searchCountry = (search.target as HTMLInputElement).value.toLowerCase();
+    console.log('Input:'+(search.target as HTMLInputElement).value);
+    console.log('Search:'+searchCountry);
     if(searchCountry === '') {
-      this.restCountriesService.getAllCountries().subscribe((data: any[]) => {
-        this.countries = data;
-      });
+      this.filteredCountries = this.countries;
       return;
     }
-    this.countries = this.countries.filter((country) => {
+    this.filteredCountries = this.countries.filter((country) => {
       return country.name.common.toLowerCase().includes(searchCountry);
     });
   }
