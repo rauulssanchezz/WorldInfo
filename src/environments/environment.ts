bad7214efc4e3b6f01923d4f70f127ad3911fa10
rauulssanchezz <rauulssanchezz@gmail.com>
2025-01-02 16:13:13 +0100

    enviroment: production: true

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
-  production: false,
+  production: true,
   newsApiKey: '5e7ef07b5a604de49a8fd901a1ce7364',
   newsApiUrl: 'https://newsapi.org/v2/everything?q=',
   restCountriesApi: 'https://restcountries.com/v3.1/'
